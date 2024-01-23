a6010a3de0c35ed8023021654061959c9704d6e2
Gary Aspinall <garyfaspinall@gmail.com>
2024-01-23 13:05:48 +1000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      'lime': '#8cae36',
+      'green1': '#3E6A31',
+      'white': '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
