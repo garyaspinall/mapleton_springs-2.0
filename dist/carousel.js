16feb634bf1a5db89cbdc20ca570ac977f5aaef0
Gary Aspinall <garyfaspinall@gmail.com>
2024-01-25 10:05:00 +1000

    Improve carousel

--- dist/carousel.js
+++ dist/carousel.js
@@ -1,19 +1,3 @@
-// window.onload = function() {
-//   const images = document.querySelectorAll("#image1, #image2, #image3, #image4");
-//   let currentImageIndex = 0;
-//   images[currentImageIndex].classList.remove("hidden");
-
-//   function cycleImages() {
-//    images[currentImageIndex].classList.add("hidden");
-//    currentImageIndex++;
-//    if (currentImageIndex >= images.length) {
-//       currentImageIndex = 0;
-//    }
-//    images[currentImageIndex].classList.remove("hidden");
-//   }
-
-//   setInterval(cycleImages, 5000);
-// };
 window.onload = function() {
   var carouselParent = document.querySelector('#carouselParent');
   var images = carouselParent.children;
