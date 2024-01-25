d5f9603c4c9bce6ca9ea0d5ac0e98ce8392ea689
Gary Aspinall <garyfaspinall@gmail.com>
2024-01-25 10:09:11 +1000

    Fix non-animated carousel

--- dist/carousel.js
+++ dist/carousel.js
@@ -1,18 +1,21 @@
 window.onload = function() {
-  var carouselParent = document.querySelector('#carouselParent');
-  var images = carouselParent.children;
-
-  let currentImageIndex = 0;
-  images[currentImageIndex].classList.remove("hidden");
-
-  function cycleImages() {
-    images[currentImageIndex].classList.add("hidden");
-    currentImageIndex++;
-    if (currentImageIndex >= images.length) {
-      currentImageIndex = 0;
-    }
-    images[currentImageIndex].classList.remove("hidden");
-  }
-
-  setInterval(cycleImages,5000);
-}
+  var carouselParents = document.querySelectorAll('.carouselParent');
+   
+  carouselParents.forEach((carouselParent) => {
+     var images = carouselParent.children;
+ 
+     let currentImageIndex = 0;
+     images[currentImageIndex].classList.remove("hidden");
+ 
+     function cycleImages() {
+       images[currentImageIndex].classList.add("hidden");
+       currentImageIndex++;
+       if (currentImageIndex >= images.length) {
+         currentImageIndex = 0;
+       }
+       images[currentImageIndex].classList.remove("hidden");
+     }
+ 
+     setInterval(cycleImages, 5000);
+  });
+ }
